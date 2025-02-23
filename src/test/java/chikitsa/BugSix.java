6fee13ac46bfaba5e7332b82f23d6f2486fbdc32
GK <gajendrakanade77@gmail.com>
2025-02-23 09:21:06 +0530

    Adding Final Scripts

--- src/test/java/chikitsa/BugSix.java
+++ src/test/java/chikitsa/BugSix.java
@@ -8,10 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.List;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
